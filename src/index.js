5a113abbba063b128a993aa28cc2aca70815dd5b
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-08-28 02:50:16 +0530

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import "./components/styles.css"
-import Navbar from './components/Navbar';
-import Body from './components/Body';
-import Footer from './components/Footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
