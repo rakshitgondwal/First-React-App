e478d122adb03f7bf1b8563dd335d3c6420e96db
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-08-30 04:51:49 +0530

    properly working, added props,maping,styles

--- src/components/format/Body.js
+++ src/components/format/Body.js
@@ -4,14 +4,24 @@ import "./styles.css"
 
 function Body(props) {
   return (
-    <div className="max-w-sm w-full lg:max-w-full lg:flex mt-4 ml-8 ">
-      <div className="shadow-md bg-white rounded-lg p-4 flex flex-col justify-between leading-normal">
-        <div className="">
-          <div className="text-gray-900 font-bold text-xl mb-2">{props.title}</div>
-          <p className="text-gray-700 text-base">{props.content}</p>
+    <div className="row">
+      <div className="column">
+
+        <div className=" mt-4 ml-4 mr-4 card">
+          <div className="shadow-md bg-white rounded-lg p-4">
+            <div className="">
+              <div className="text-gray-900 font-bold text-xl mb-2">{props.title}</div>
+              <p className="text-gray-700 text-base">{props.content}</p>
+            </div>
+          </div>
         </div>
+
       </div>
     </div>
+
+
+
+
   );
 }
 
